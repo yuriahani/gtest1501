f5cc0f76e1fcab3d6a9807cda0b0b540d8d82686
Ari Rantala <Ari.Rantala@netisto.fi>
2017-01-15 19:47:35 +0200

    Eka commit

---
+++ gtest1501/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FirstTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // määritellään muuttuja
+            string nimi = "Matti Mainio";
+            // tulostetaan muuttujan arvo konsolille
+            Console.WriteLine("Nimi on " + nimi);
+            // pidetään sovelluksen ajoikkuna näytöllä. Sulkeutuu, kun enter-painiketta on painettu
+            Console.ReadLine();
+        }
+    }
+}
